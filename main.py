88792ceb7cad0939786e88ce48a687d881ec8429
DESKTOP-GRBB3NJ\Kayque <kayque.original@hotmail.com>
2025-11-06 14:54:07 -0300

    Adição de nova função para obter informações genéricas do repositório

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import typer
-from src.commits_info import show_commits_info
+from src.commits_info import show_commits_info, show_repository_generic_info
 from src.loc_analysis import analyze_loc
 
 app = typer.Typer(help="Ferramenta CLI para mineração de repositórios de software.")
@@ -19,6 +19,14 @@ def loc(repo_url: str, from_commit: str, to_commit: str):
     """
     typer.echo(f"Analisando LOC do repositório: {repo_url}")
     analyze_loc(repo_url, from_commit, to_commit)
+    
+@app.command()
+def generic(repo_url: str):
+    """
+    Mostra informações genéricas de um repositório.
+    """
+    typer.echo(f"Analisando informações do repositório: {repo_url}")
+    show_repository_generic_info(repo_url)
 
 if __name__ == "__main__":
     app()
